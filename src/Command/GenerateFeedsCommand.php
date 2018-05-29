756fe16a5ce40bc57aeefad1a8b9f70b699c1b74
Joachim Løvgaard <joachim@loevgaard.dk>
2018-05-29 11:20:53 +0200

    added more fields to the xml output

--- src/Command/GenerateFeedsCommand.php
+++ src/Command/GenerateFeedsCommand.php
@@ -5,23 +5,31 @@ declare(strict_types=1);
 namespace Loevgaard\SyliusFeedPlugin\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
 use Loevgaard\SyliusFeedPlugin\Entity\FeedInterface;
 use Loevgaard\SyliusFeedPlugin\Repository\FeedRepositoryInterface;
 use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Core\Model\ChannelPricingInterface;
+use Sylius\Component\Core\Model\ImageInterface;
+use Sylius\Component\Core\Model\ProductImageInterface;
 use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ProductVariant;
+use Sylius\Component\Core\Model\TaxonInterface;
 use Sylius\Component\Core\Repository\ProductRepositoryInterface;
 use Sylius\Component\Locale\Model\LocaleInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * TODO
- * * Use XML lib for writing xml file, i.e. http://sabre.io/xml/ or https://github.com/servo-php/fluidxml
  * * Use Gaufrette file system abstraction instead of Symfonys
  * * Use a product repository trait to fetch products for the respective channel and locale
  */
@@ -57,13 +65,25 @@ final class GenerateFeedsCommand extends ContainerAwareCommand
      */
     private $router;
 
+    /**
+     * @var CacheManager
+     */
+    private $imagineCacheManager;
+
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
     public function __construct(
         FeedRepositoryInterface $feedRepository,
         ChannelRepositoryInterface $channelRepository,
         ProductRepositoryInterface $productRepository,
         EntityManagerInterface $entityManager,
         Filesystem $filesystem,
-        RouterInterface $router
+        RouterInterface $router,
+        CacheManager $imagineCacheManager,
+        TranslatorInterface $translator
     ) {
         parent::__construct();
 
@@ -73,6 +93,8 @@ final class GenerateFeedsCommand extends ContainerAwareCommand
         $this->productManager = $entityManager;
         $this->filesystem = $filesystem;
         $this->router = $router;
+        $this->imagineCacheManager = $imagineCacheManager;
+        $this->translator = $translator;
     }
 
     protected function configure(): void
@@ -102,6 +124,9 @@ final class GenerateFeedsCommand extends ContainerAwareCommand
 
     private function generateFeed(FeedInterface $feed, ChannelInterface $channel, LocaleInterface $locale) : void
     {
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
+        $this->translator->setLocale($locale->getCode());
+
         // configure router context
         $context = $this->router->getContext();
         $context->setHost($channel->getHostname());
@@ -116,33 +141,159 @@ final class GenerateFeedsCommand extends ContainerAwareCommand
         $xmlWriter->openURI($tmpFile->getPathname());
         $xmlWriter->setIndent(true);
         $xmlWriter->setIndentString('  ');
-        $xmlWriter->startDocument();
+        $xmlWriter->startDocument('1.0', 'UTF-8');
         $xmlWriter->startElement('products');
 
         foreach ($products as $product) {
+            if(!$product->isEnabled()) {
+                continue;
+            }
+
             $link = $this->router->generate('sylius_shop_product_show', ['_locale' => $locale->getCode(), 'slug' => $product->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL);
 
-            // outer product tag begin
-            $xmlWriter->startElement('product');
+            // deduce values based on variants
+            $stock = 0;
+            $lowestPrice = PHP_INT_MAX;
+            $highestPrice = 0;
 
-            // add data tags
-            $xmlWriter->writeElement('id', (string)$product->getId());
-            $xmlWriter->writeElement('code', $product->getCode());
-            $xmlWriter->writeElement('link', $link);
-            $xmlWriter->writeElement('name', $product->getName());
-            $xmlWriter->writeElement('short_description', $product->getShortDescription());
-            $xmlWriter->writeElement('description', $product->getDescription());
-            $xmlWriter->writeElement('main_taxon', $product->getMainTaxon() ? $product->getMainTaxon()->getName() : '');
-            $xmlWriter->writeElement('created_at', $product->getCreatedAt()->format(DATE_ATOM));
-            $xmlWriter->writeElement('updated_at', $product->getUpdatedAt()->format(DATE_ATOM));
+            foreach ($product->getVariants() as $variant) {
+                /** @var ProductVariant $variant */
+                $stock += $variant->getOnHand();
 
-            // outer product tag end
-            $xmlWriter->endElement();
+                $prices = $variant->getChannelPricings();
+                foreach ($prices as $price) {
+                    /** @var ChannelPricingInterface $price */
 
-            echo (memory_get_usage(true) / 1024 / 1024)." MB\n";
+                    if($price->getPrice() < $lowestPrice) {
+                        $lowestPrice = $price->getPrice();
+                    }
 
-            // todo flush when we've reached a certain threshold of PHP's max memory limit
-            //$xmlWriter->flush();
+                    if($price->getPrice() > $highestPrice) {
+                        $highestPrice = $price->getPrice();
+                    }
+                }
+            }
+
+            $data = [
+                'id' => $product->getId(),
+                'code' => $product->getCode(),
+                'link' => $link,
+                'name' => $product->getName(),
+                'short_description' => $product->getShortDescription(),
+                'description' => $product->getDescription(),
+                'condition' => 'new',
+                'stock' => $stock,
+                'in_stock' => $stock > 0,
+                'lowest_price' => $lowestPrice,
+                'highest_price' => $highestPrice,
+                'currency' => $channel->getBaseCurrency()->getCode(),
+                'is_master' => true,
+                'is_variant' => false,
+                'variant_group_id' => $product->getCode(),
+                'created_at' => $product->getCreatedAt()->format(DATE_ATOM),
+                'updated_at' => $product->getUpdatedAt()->format(DATE_ATOM),
+            ];
+
+            $mainTaxonPath = null;
+
+            if($product->getMainTaxon()) {
+                $data['main_taxon'] = $product->getMainTaxon()->getName();
+                $mainTaxonPath = $product->getMainTaxon()->getName();
+
+                foreach ($product->getMainTaxon()->getAncestors() as $ancestor) {
+                    $mainTaxonPath = $ancestor->getName().' > '.$mainTaxonPath;
+                }
+
+                $data['main_taxon_path'] = $this->translator->trans('sylius.ui.home').' > '.$mainTaxonPath;
+            }
+
+            $image = null;
+            foreach ($product->getImagesByType('main') as $image) {
+                $image = $this->imagineCacheManager->getBrowserPath($image->getPath(), 'sylius_shop_product_original');
+            }
+
+            if($image) {
+                $data['image'] = $image;
+            }
+
+            $gtin = null;
+            try {
+                $gtin = $propertyAccessor->getValue($product, 'gtin');
+            } catch (NoSuchPropertyException $e) {}
+            if ($gtin) {
+                $data['gtin'] = $gtin;
+            }
+
+            $brand = null;
+            try {
+                $brand = $propertyAccessor->getValue($product, 'brand');
+            } catch (NoSuchPropertyException $e) {}
+            if ($brand) {
+                $data['brand'] = $brand;
+            }
+
+            $this->writeProduct($xmlWriter, $data);
+
+            foreach ($product->getVariants() as $variant) {
+                /** @var ProductVariant $variant */
+
+                $data = [
+                    'id' => $variant->getId(),
+                    'code' => $variant->getCode(),
+                    'link' => $link,
+                    'name' => $variant->getName(),
+                    'short_description' => $product->getShortDescription(),
+                    'description' => $product->getDescription(),
+                    'condition' => 'new',
+                    'stock' => $stock,
+                    'in_stock' => $stock > 0,
+                    'currency' => $channel->getBaseCurrency()->getCode(),
+                    'is_master' => false,
+                    'is_variant' => true,
+                    'variant_group_id' => $product->getCode(),
+                    'created_at' => $variant->getCreatedAt()->format(DATE_ATOM),
+                    'updated_at' => $variant->getUpdatedAt()->format(DATE_ATOM),
+                ];
+
+                if($product->getMainTaxon()) {
+                    $data['main_taxon'] = $product->getMainTaxon()->getName();
+                }
+
+                if($mainTaxonPath) {
+                    $data['main_taxon_path'] = $mainTaxonPath;
+                }
+
+                if($image) {
+                    $data['image'] = $image;
+                }
+
+                try {
+                    $gtin = $propertyAccessor->getValue($variant, 'gtin');
+                    if ($gtin) {
+                        $data['gtin'] = $gtin;
+                    }
+                } catch (NoSuchPropertyException $e) {
+
+                }
+
+                if ($brand) {
+                    $data['brand'] = $brand;
+                }
+
+                $prices = $variant->getChannelPricings();
+                $basePrice = null;
+                foreach ($prices as $price) {
+                    /** @var ChannelPricingInterface $price */
+
+                    $basePrice = $price->getPrice();
+                }
+
+                if($basePrice) {
+                    $data['price'] = $basePrice;
+                }
+
+                $this->writeProduct($xmlWriter, $data);
+            }
         }
 
         $xmlWriter->endElement();
@@ -154,6 +305,25 @@ final class GenerateFeedsCommand extends ContainerAwareCommand
         $this->filesystem->mkdir($dir);
 
         $this->filesystem->rename($tmpFile->getPathname(), $dir.'/'.$feed->getSlug().'.xml', true);
+    }
+
+    private function writeProduct(\XMLWriter $xmlWriter, array $data) : void
+    {
+        $xmlWriter->startElement('product');
+
+        foreach ($data as $key => $val) {
+            if(is_bool($val)) {
+                $val = $this->boolToString($val);
+            }
+
+            $xmlWriter->writeElement($key, (string)$val);
+        }
+
+        $xmlWriter->endElement();
+    }
+
+    private function boolToString(bool $val) : string {
+        return $val ? 'true' : 'false';
     }
 
     private function getTmpFile() : \SplFileInfo
