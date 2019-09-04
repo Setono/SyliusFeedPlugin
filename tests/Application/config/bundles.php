24555ead2011b4796b0f66335a644dbc707d3a34
Joachim Løvgaard <joachim@loevgaard.dk>
2019-09-04 14:07:29 +0200

    Upgraded to Sylius v1.6

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -58,4 +58,5 @@ return [
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true, 'test_cached' => true],
 ];
