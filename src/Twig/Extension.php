317ba66d28454ba3b0049e84a1c8c5763a80d2fe
Joachim Løvgaard <joachim@loevgaard.dk>
2019-09-10 10:17:20 +0200

    Added serializer to aid in debugging a feed

--- src/Twig/Extension.php
+++ src/Twig/Extension.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Setono\SyliusFeedPlugin\Twig;
 
+use const JSON_PRESERVE_ZERO_FRACTION;
+use const JSON_PRETTY_PRINT;
+use const JSON_UNESCAPED_SLASHES;
+use const JSON_UNESCAPED_UNICODE;
 use Safe\Exceptions\PcreException;
 use function Safe\preg_replace;
 use Setono\SyliusFeedPlugin\Model\FeedInterface;
@@ -11,6 +15,8 @@ use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Locale\Model\LocaleInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
+use Symfony\Component\Serializer\SerializerInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
@@ -23,16 +29,24 @@ final class Extension extends AbstractExtension
     /** @var UrlGeneratorInterface */
     private $urlGenerator;
 
-    public function __construct(RequestStack $requestStack, UrlGeneratorInterface $urlGenerator)
-    {
+    /** @var SerializerInterface */
+    private $serializer;
+
+    public function __construct(
+        RequestStack $requestStack,
+        UrlGeneratorInterface $urlGenerator,
+        SerializerInterface $serializer
+    ) {
         $this->requestStack = $requestStack;
         $this->urlGenerator = $urlGenerator;
+        $this->serializer = $serializer;
     }
 
     public function getFilters(): array
     {
         return [
             new TwigFilter('setono_sylius_feed_remove_empty_tags', [$this, 'removeEmptyTags']),
+            new TwigFilter('setono_sylius_feed_json', [$this, 'json']),
         ];
     }
 
@@ -49,6 +63,16 @@ final class Extension extends AbstractExtension
     public function removeEmptyTags(string $xml): string
     {
         return preg_replace('#<[^/>][^>]*></[^>]+>#', '', $xml);
+    }
+
+    /**
+     * @param array|object|mixed $data
+     */
+    public function json($data): string
+    {
+        return $this->serializer->serialize($data, 'json', [
+            JsonEncode::OPTIONS => JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION,
+        ]);
     }
 
     public function generateFeedUrl(FeedInterface $feed, ChannelInterface $channel, LocaleInterface $locale): string
