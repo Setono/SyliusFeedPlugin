b8e8cec1b1dd95c3f0a56f16bc06b801e824c7e4
Joachim Løvgaard <joachim@loevgaard.dk>
2019-12-13 12:57:48 +0100

    Added in stock filter

---
+++ src/EventListener/Filter/InStockFilterListener.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusFeedPlugin\EventListener\Filter;
+
+use Safe\Exceptions\StringsException;
+use function Safe\sprintf;
+use Setono\SyliusFeedPlugin\Event\QueryBuilderEvent;
+use Sylius\Component\Inventory\Model\StockableInterface;
+
+final class InStockFilterListener extends AbstractFilterListener
+{
+    /**
+     * @throws StringsException
+     */
+    public function filter(QueryBuilderEvent $event): void
+    {
+        if (!$this->isEligible($event, [StockableInterface::class])) {
+            return;
+        }
+
+        if (!$this->getClassMetadata($event)->hasField('onHand')) {
+            return;
+        }
+
+        $qb = $event->getQueryBuilder();
+
+        $alias = $this->getAlias($qb);
+
+        $event->getQueryBuilder()->andWhere(sprintf('%s.onHand > 0', $alias));
+    }
+}
