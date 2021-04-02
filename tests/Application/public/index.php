5583f55959de10611905721ffd642bdaa5b24124
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2021-04-02 12:35:50 +0200

    Add compatibility with Sylius 1.9 and Symfony 5.2

--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\HttpFoundation\Request;
 use Tests\Setono\SyliusFeedPlugin\Application\Kernel;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__) . '/config/bootstrap.php';
 
