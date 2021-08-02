0cabbcb77eb434a13401d866c654c095518a8605
Joachim Løvgaard <joachim@loevgaard.dk>
2021-08-02 08:25:32 +0200

    Fix bug in Sylius' coding standard

--- ecs.php
+++ ecs.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
@@ -14,13 +13,4 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->parameters()->set(Option::SKIP, [
         'tests/Application/**',
     ]);
-
-    /**
-     * Was added to fix this exception:
-     *
-     * PhpCsFixer\ConfigurationException\InvalidFixerConfigurationException: [binary_operator_spaces] Invalid configuration:
-     * The options "align_double_arrow", "align_equals" do not exist. Defined options are: "default", "operators".
-     * in vendor/friendsofphp/php-cs-fixer/src/AbstractFixer.php on line 155
-     */
-    $containerConfigurator->services()->set(BinaryOperatorSpacesFixer::class);
 };
