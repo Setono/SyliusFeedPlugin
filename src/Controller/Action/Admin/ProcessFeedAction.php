b25400d84d57388d588649c2ae4d73c252de54b7
Arjan Frans <arjan.frans@fusonic.net>
2021-11-05 10:55:27 +0100

    Do not process already processing feeds

--- src/Controller/Action/Admin/ProcessFeedAction.php
+++ src/Controller/Action/Admin/ProcessFeedAction.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace Setono\SyliusFeedPlugin\Controller\Action\Admin;
 
+use Doctrine\Persistence\ObjectManager;
 use Setono\SyliusFeedPlugin\Message\Command\ProcessFeed;
+use Setono\SyliusFeedPlugin\Model\Feed;
+use Setono\SyliusFeedPlugin\Repository\FeedRepositoryInterface;
+use Setono\SyliusFeedPlugin\Workflow\FeedGraph;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Webmozart\Assert\Assert;
 
 final class ProcessFeedAction
 {
@@ -20,21 +25,30 @@ final class ProcessFeedAction
     private FlashBagInterface $flashBag;
 
     private TranslatorInterface $translator;
+    private FeedRepositoryInterface $feedRepository;
 
     public function __construct(
         MessageBusInterface $commandBus,
         UrlGeneratorInterface $urlGenerator,
         FlashBagInterface $flashBag,
+        FeedRepositoryInterface $feedRepository,
         TranslatorInterface $translator
     ) {
         $this->commandBus = $commandBus;
         $this->urlGenerator = $urlGenerator;
         $this->flashBag = $flashBag;
         $this->translator = $translator;
+        $this->feedRepository = $feedRepository;
     }
 
     public function __invoke(int $id): RedirectResponse
     {
+        /** @var Feed|null $feed */
+        $feed = $this->feedRepository->find($id);
+
+        Assert::notNull($feed);
+        Assert::notEq($feed->getState(), FeedGraph::STATE_PROCESSING);
+
         $this->commandBus->dispatch(new ProcessFeed($id));
 
         $this->flashBag->add('success', $this->translator->trans('setono_sylius_feed.feed_generation_triggered'));
--- src/Processor/FeedProcessor.php
+++ src/Processor/FeedProcessor.php
@@ -8,6 +8,7 @@ use Psr\Log\LoggerAwareTrait;
 use Psr\Log\NullLogger;
 use Setono\SyliusFeedPlugin\Message\Command\ProcessFeed;
 use Setono\SyliusFeedPlugin\Repository\FeedRepositoryInterface;
+use Setono\SyliusFeedPlugin\Workflow\FeedGraph;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 final class FeedProcessor implements FeedProcessorInterface
@@ -30,8 +31,16 @@ final class FeedProcessor implements FeedProcessorInterface
         $feeds = $this->feedRepository->findEnabled();
 
         foreach ($feeds as $feed) {
-            $this->logger->info(sprintf('Triggering processing for feed "%s" (id: %d)', (string) $feed->getName(), (int) $feed->getId()));
-            $this->commandBus->dispatch(new ProcessFeed((int) $feed->getId()));
+            if ($feed->getState() !== FeedGraph::STATE_PROCESSING) {
+                $this->logger->info(
+                    sprintf(
+                        'Triggering processing for feed "%s" (id: %d)',
+                        (string)$feed->getName(),
+                        (int)$feed->getId()
+                    )
+                );
+                $this->commandBus->dispatch(new ProcessFeed((int)$feed->getId()));
+            }
         }
     }
 }
