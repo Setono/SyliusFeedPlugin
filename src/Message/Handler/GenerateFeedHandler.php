3e49044f096bb7fa50e98cd370fd7f17614f18fd
Fabiana Romagnoli <fromagnoli@webgriffe.com>
2022-01-24 11:40:14 +0100

    Fix ecs

--- src/Message/Handler/GenerateFeedHandler.php
+++ src/Message/Handler/GenerateFeedHandler.php
@@ -20,7 +20,9 @@ use Symfony\Component\Messenger\MessageBusInterface;
 final class GenerateFeedHandler implements MessageHandlerInterface
 {
     use GetChannelTrait;
+
     use GetFeedTrait;
+
     use GetLocaleTrait;
 
     private FeedTypeRegistryInterface $feedTypeRegistry;
