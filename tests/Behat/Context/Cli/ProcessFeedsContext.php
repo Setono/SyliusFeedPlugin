784eadc917ff8296156daf410364e4295abd6372
Manuele Menozzi <manuele.menozzi@gmail.com>
2022-06-15 07:38:40 +0200

    Fix Behat scenario

--- tests/Behat/Context/Cli/ProcessFeedsContext.php
+++ tests/Behat/Context/Cli/ProcessFeedsContext.php
@@ -125,7 +125,6 @@ final class ProcessFeedsContext implements Context
     <g:image_link>https://example.dk/media/cache/resolve/sylius_shop_product_large_thumbnail/%image_path%</g:image_link>
     <g:availability>in_stock</g:availability>
     <g:price>0USD</g:price>
-    <g:identifier_exists>false</g:identifier_exists>
     <g:condition>new</g:condition>
     <g:item_group_id>WARM_BEER</g:item_group_id>
 </item>
@@ -148,7 +147,6 @@ CONTENT;
     <g:image_link>https://example.com/media/cache/resolve/sylius_shop_product_large_thumbnail/%image_path%</g:image_link>
     <g:availability>in_stock</g:availability>
     <g:price>0USD</g:price>
-    <g:identifier_exists>false</g:identifier_exists>
     <g:condition>new</g:condition>
     <g:item_group_id>COLD_BEER</g:item_group_id>
 </item>
