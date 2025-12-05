997262c50c23eb72da308c26ae5ff5dfa4367be4
Joachim Løvgaard <joachim@loevgaard.dk>
2025-12-05 11:46:29 +0100

    Fix deps

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,15 @@
 <?php
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
-;
+    ->ignoreUnknownClasses([
+        League\Flysystem\FileNotFoundException::class,
+        League\Flysystem\FilesystemInterface::class,
+        League\Flysystem\RootViolationException::class,
+    ])
+    ->ignoreErrorsOnPackage('league/flysystem-bundle', [ErrorType::UNUSED_DEPENDENCY]) // this is used to inject the filesystem service
+    ->ignoreErrorsOnPackage('setono/doctrine-orm-batcher-bundle', [ErrorType::UNUSED_DEPENDENCY]) // this is used to inject the batcher service
+    ;
